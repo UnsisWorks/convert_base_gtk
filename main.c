2c4d3b0a1b7da5852ab2c15b6adc3167cd89a699
unsis <jared0192galicia@gmail.com>
2022-10-07 16:35:43 -0500

    Construct UI

--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *fixed;
+    gtkWidgrt *labelBin, *labelDec, *labelOcta, *labelhexa;
     GtkWidget *txtBin, *txtOcta, *txtHexa, *txtDec;
     GtkWidget *buttonBox, button;
 
@@ -14,16 +15,28 @@ static void activate(GtkApplication *app, gpointer user_data) {
     button = gtk_button_new_with_label("Calcular");
     gtk_container_add(GTK_CONTAINER(buttonBox), button);
     gtk_fixed_put(GTK_FIXED(fixed), buttonBox, 140, 500);
-    // Create entry text
+
+    // Create entry text with label 
     txtBin = gtk_entry_new();
+    labelBin = gtk_label_new("Binario");
+
     txtOcta = gtk_entry_new();
+    labelOctal = gtk_label_new("Octal");
+    labelOcta = gtk
     txtHexa = gtk_entry_new();
+    labelHexa = gtk_label_new("Hexadecimal");
     txtDec = gtk_entry_new();
+    labelDecl = gtk_label_new("decimal");
 
     gtk_fixed_put(GTK_FIXED(fixed), txtBin, 120, 100);
     gtk_fixed_put(GTK_FIXED(fixed), txtOcta, 120, 200);
     gtk_fixed_put(GTK_FIXED(fixed), txtDec, 120, 300);
     gtk_fixed_put(GTK_FIXED(fixed), txtHexa, 120, 400);
+
+    gtk_fixed_put(GTK_FIXED(fixed), txtBin, 130, 100);
+    gtk_fixed_put(GTK_FIXED(fixed), txtDec, 130, 300);
+    gtk_fixed_put(GTK_FIXED(fixed), txtHexa, 130, 400);
+    gtk_fixed_put(GTK_FIXED(fixed), txtOct, 130, 200);
 
     window = gtk_application_window_new(app);
     gtk_window_set_title(GTK_WINDOW(window), "Window");
