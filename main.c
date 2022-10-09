b674fdf1d5988f4bbab96fb8b377e0647e41a53b
jared0192galicia <jared0192galicia@gmail.com>
2022-10-09 03:35:41 -0500

    identifica digitos (octal funtion)

--- main.c
+++ main.c
@@ -25,19 +25,39 @@ static void sendMessage (GtkWidget *widget, gchar *message, gchar *title) {
     gtk_container_add(GTK_CONTAINER(contentArea), label);
     gtk_widget_show_all(dialog);
 }
-static void convert (int id, GString *value) {
 
+// Convert input a bases rest
+static void convert (int id, GString *value) {
+    gboolean flag = TRUE;
     // Selected convert correct
     switch (id) {
         // Cast from binary to rest bases
         case 0:
-            g_print("action\n");
-            for (gint i = 1; i <= value->len; i++) {
-                g_print("%d\n", i);
+            // Iterat value (except char end string '\0')
+            for (gint i = 0; i <= (value->len) - 1; i++) {
+                // Compare chars the object string with '0', '1' y '.'
+                if (!((value->str[i] == '0') || value->str[i] == '1' || value->str[i] == '.')) {
+                    flag = FALSE;
+                    break;
+                }
+            }
+
+            if (flag) {
+                g_print("continue\n");
+                /* Codificacion de base */
+            } else {
+                sendMessage(NULL, "Ingresar unicamente 0 y 1", "Aviso");
             }
             break;
         case 1:
-            g_print("Octal\n");
+            g_print("before bucle octal\n");
+            for (gint i = 0; i <= (value->len) - 1; i++) {
+                if (g_ascii_isdigit(value->str[i])) {
+                    g_print("Digito\n");
+                } else {
+                    g_print("Falso\n");
+            } 
+            }
             break;
         case 2:
             g_print("Hexa\n");
@@ -49,8 +69,9 @@ static void convert (int id, GString *value) {
             break;
     }
 }
+
+// Funtion execute press button 'convertir'
 static void calcular(GtkWidget *widget, gpointer data) {
-    
     int count = 0;
     int id = 0;
     const gchar *value;
@@ -68,16 +89,19 @@ static void calcular(GtkWidget *widget, gpointer data) {
      if ((strcmp(octal, "") != 0)) {
         count++;
         id = 1;
+        value = octal;
     }
      if ((strcmp(hexa, "") != 0)) {
         count++;
         id = 2;
+        value = hexa;
     }
      if ((strcmp(deci, "") != 0)) {
         count++;
         id = 3;
+        value = deci;
     }
-
+    // num inputs need?
     if (count == 1) {
         GString *str = g_string_new(value);
         convert(id, str);
@@ -85,7 +109,7 @@ static void calcular(GtkWidget *widget, gpointer data) {
         sendMessage(widget, "Solo debe ingresar un dato", "Advertencia");
     }
 }
-
+// Funtion construct
 static void activate(GtkApplication *app, gpointer user_data) {
     GtkWidget *window;
     GtkWidget *fixed;
@@ -102,7 +126,7 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
     gtk_container_add(GTK_CONTAINER(buttonBox), button);
     // add button and set position in fixed 
-    gtk_fixed_put(GTK_FIXED(fixed), buttonBox, 170, 500);
+    gtk_fixed_put(GTK_FIXED(fixed), buttonBox, 190, 500);
 
     // Create entry text with label 
     txtBin = gtk_entry_new();
@@ -110,11 +134,14 @@ static void activate(GtkApplication *app, gpointer user_data) {
 
     txtOcta = gtk_entry_new();
     labelOcta = gtk_label_new("Octal");
+
     txtHexa = gtk_entry_new();
     labelHexa = gtk_label_new("Hexadecimal");
+
     txtDec = gtk_entry_new();
     labelDec = gtk_label_new("decimal");
 
+    // Add witgets at fixed with position
     gtk_fixed_put(GTK_FIXED(fixed), txtBin, 220, 100);
     gtk_fixed_put(GTK_FIXED(fixed), txtOcta, 220, 200);
     gtk_fixed_put(GTK_FIXED(fixed), txtDec, 220, 300);
@@ -143,6 +170,7 @@ int main(int argc, char **argv) {
     GtkApplication *app;
     int status;
 
+    // init window at app
     app = gtk_application_new("org.gtk.example", G_APPLICATION_FLAGS_NONE);
     g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
     status = g_application_run(G_APPLICATION(app), argc, argv);
