5cf76a37e82f6fbad98ef72fbdc773069dae76b0
unsis <jared0192galicia@gmail.com>
2022-10-11 18:47:00 -0500

    Convert before point to decimal

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <gtk/gtk.h>
 #include <stdio.h>
+#include <math.h>
 
 GtkWidget *txtBin, *txtOcta, *txtHexa, *txtDec;
 gchar letters[] = {'A', 'B', 'C', 'D', 'E', 'F'};
@@ -9,25 +10,37 @@ void binToDecimal(const gchar *reply, GString *value){
     GString *afterPoint = g_string_new(value -> str);
     GString *beforePoint = g_string_new(value -> str);
 
+    gboolean flag = FALSE;
     gint after = 0;
     gint before = 0;
+    gint pot = 0;
+    gfloat result = 0;
 
     // Separete string for point
     for (gint i = 0; i < value->len; i++) {
         if (value -> str[i] == '.'){
             beforePoint = g_string_truncate(beforePoint, i);
             afterPoint = g_string_erase(afterPoint, 0, i + 1);
-        }
+            flag = TRUE;
+            break;
+        } 
     }
     g_print("before: %s   After: %s  \n", beforePoint -> str, afterPoint -> str);
 
     // Convert part before at point
     for (gint i = (beforePoint -> len) - 1; i >= 0; i--) {
         if (beforePoint -> str[i] == '1') {
-            g_print("value: %c  pot: %ld \n", (beforePoint -> str[i]), (beforePoint->len - 1) - i);
-            
-
+            pot = (beforePoint->len - 1) - i;
+            g_print("value: %c  pot: %d  ", (beforePoint -> str[i]), pot);
+            after = pow(2, pot);
+            result += after;
+            g_print(" =  %d +=  %f\n", after, result);
         }
+    }
+
+    if (flag) {
+        // Create cod after point if exist
+        
     }
 }
 
@@ -269,5 +282,5 @@ int main(int argc, char **argv) {
     return status;
 }
 
-// Compiler: gcc `pkg-config --cflags gtk+-3.0` -o auto main.c `pkg-config --libs gtk+-3.0`
+// Compiler: gcc `pkg-config --cflags gtk+-3.0` -o auto main.c `pkg-config --libs gtk+-3.0` -lm
 // https://calcifer.org/documentos/librognome/glib-strings.html 
